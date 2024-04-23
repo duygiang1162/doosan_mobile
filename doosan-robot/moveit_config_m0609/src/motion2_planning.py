c639a1610460d273d9c2a52b887db565a6f64e23
duygiang <ddhduygiang@gmail.com>
2024-04-23 10:07:34 +0700

    fix ignore Cmake

--- doosan-robot/moveit_config_m0609/src/motion2_planning.py
+++ doosan-robot/moveit_config_m0609/src/motion2_planning.py
@@ -13,7 +13,7 @@ joints = ["joint1", "joint2", "joint3", "joint4", "joint5", "joint6"]
 pose1 = [0.0, 0.0, 0.0, -1.57, 0.0, 1.57, 0.0]
 target =	{
     "home": [-1.57, 0.0, 1.57, 0.0, 1.57, 0.0, 0.0],
-    "pose_1": [-1.0472, 0.7165, 1.4181, 0.248, 1.0748, 0.0, 0.0],
+    "pose_1": [-1.0472, 0.6065, 1.4181, 0.248, 1.0748, 0.0, 0.0],
     "pose_2": [-1.0472, 0.7165, 1.4181, 1.6572, 1.0748, 0.0],
     "pose_3": [-1.0472, 0.7165, 1.4181, 1.6572, -0.2038, 0.0],
     "pose_4": [0.0443, 0.2747, 1.3552, 1.6572, -1.6749, 0.0],
@@ -26,6 +26,12 @@ while not rospy.is_shutdown():
         if result:
             if result.error_code.val == MoveItErrorCodes.SUCCESS:
                 rospy.loginfo(f"Trajectory {pose_name} successfully executed!")
+                rospy.loginfo("Current Joint Values:")
+                # print(moveit_commander.MoveGroupCommander("arm").get_current_joint_values())
+                joint_values=moveit_commander.MoveGroupCommander("arm").get_current_joint_values()
+                rounded_joint_values = [round(value, 2) for value in joint_values]
+
+                rospy.loginfo(rounded_joint_values)
             else:
                 rospy.logerr("Arm goal in state: %s", move_group.get_move_action().get_state())
         else:
